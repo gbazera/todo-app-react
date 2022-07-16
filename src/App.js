c82819428ab663c9af90651b29994300574908b8
gbazera <bazerashvili.gio27@gmail.com>
2022-07-16 15:41:02 +0400

    Added routes (not working)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Router, Route, Link } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Board from './components/Board'
+import Auth from './components/Auth'
 
 function App() {
 	return (
-		<div className="App">
-			<Sidebar />
-			<Board />
-		</div>
+		<Router>
+			<div className="App">
+				<Link to="/">Home</Link>
+				<Link to="/auth">Auth</Link>
+				<Route exact path="/" element={<Sidebar />} />
+				<Route exact path="/" element={<Board />} />
+				<Route exact path="/auth" element={<Auth />} />
+			</div>
+		</Router>
 	);
 }
 
---
+++ src/components/Auth.jsx
@@ -0,0 +1,9 @@
+function Auth(){
+    return(
+        <div className="auth-panel">
+            <button className="btn"><i className="bx bxl google"></i> SIGN IN</button>
+        </div>
+    );
+}
+
+export default Auth;
